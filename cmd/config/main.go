852078aa8d5b6e7ca15034ead1f920546b091d21
Anna Shaleva <shaleva.ann@nspcc.ru>
2021-05-19 14:59:36 +0300

    cmd: allow to log network utilisation

--- cmd/config/main.go
+++ cmd/config/main.go
@@ -331,6 +331,7 @@ func generateCompose(n int) error {
 			Volumes: []string{
 				fmt.Sprintf("./go.protocol.privnet.%d.yml:/config/protocol.privnet.yml", i+1),
 				"./logs/:/logs/",
+				"./logs_network_utilisation/:/logs_utilisation/",
 			},
 		}
 		if i == 0 || i == 4 || i == 9 || i == 19 || i == 29 || i == 49 || i == 74 || i == 99 {
